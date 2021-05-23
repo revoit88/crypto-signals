ee6a2547d64e6e0bcef6c0852c068485ee9c9e54
Jesus Jimenez <jesuswebdev@gmail.com>
2021-05-23 16:06:28 -0400

    use env variables in init candles script

--- utils/init_candles.js
+++ utils/init_candles.js
@@ -3,8 +3,11 @@ const pairs = require("./pairs");
 
 const init = async () => {
   for (const pair of pairs) {
+    console.log("current pair: ", pair.symbol);
     await axios.post(
-      `http://localhost:8080/candles/binance?symbol=${pair.symbol}`
+      `${process.env.API_URL}/candles/binance?symbol=${pair.symbol}`,
+      {},
+      { headers: { Authorization: `Bearer ${process.env.TOKEN}` } }
     );
   }
 };

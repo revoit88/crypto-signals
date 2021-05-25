689f982474630b78e903daa8144538aba2b0b370
Jesus Jimenez <jesuswebdev@gmail.com>
2021-05-25 17:48:30 -0400

    remove tojson

--- candles-processor/app.js
+++ candles-processor/app.js
@@ -45,8 +45,8 @@ const init = async () => {
             ]
           })
             .hint("symbol_1_open_time_1")
-            .sort({ open_time: 1 })
-            .then(found => found.map(c => c.toJSON()));
+            .sort({ open_time: 1 });
+          // .then(found => found.map(c => c.toJSON()));
 
           if (candles.length >= 150) {
             const ohlc = getOHLCValues(candles);

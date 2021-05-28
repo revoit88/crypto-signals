2b00db882bffa2a6796ea70116eea70760bc3b81
Jesus Jimenez <jesuswebdev@gmail.com>
2021-05-28 19:37:20 -0400

    add force variable to init candles script

--- utils/init_candles.js
+++ utils/init_candles.js
@@ -2,10 +2,13 @@ const axios = require("axios");
 const pairs = require("./pairs");
 
 const init = async () => {
+  const force = process.env.FORCE;
   for (const pair of pairs) {
     console.log("current pair: ", pair.symbol);
     await axios.post(
-      `${process.env.API_URL}/candles/binance?symbol=${pair.symbol}`,
+      `${process.env.API_URL}/candles/binance?symbol=${pair.symbol}${
+        force ? "&force=true" : ""
+      }`,
       {},
       { headers: { Authorization: `Bearer ${process.env.TOKEN}` } }
     );

1fb88db7ccb328fc0201a766b4a01f9a3c781e90
Jesus Jimenez <jesuswebdev@gmail.com>
2021-07-10 17:17:34 -0400

    broadcast signals to default to false on market creation

--- api/src/market/model.js
+++ api/src/market/model.js
@@ -16,7 +16,7 @@ const MarketSchema = new Schema(
     trader_lock: { type: Boolean },
     last_trader_lock_update: { type: Number },
     trading: { type: Boolean, default: false },
-    broadcast_signals: { type: Boolean, default: true },
+    broadcast_signals: { type: Boolean, default: false },
     send_to_profit_sharing: { type: Boolean, default: false },
     use_main_account: { type: Boolean, default: false }
   },

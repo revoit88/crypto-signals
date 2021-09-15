6074ead5c1890b51eaf9ba1b39e0d86508cb01d5
Jesús Jiménez <jesuswebdev@gmail.com>
2021-09-15 16:32:27 -0400

    add more text to the about page

--- website/src/views/about/index.js
+++ website/src/views/about/index.js
@@ -8,7 +8,7 @@ const About = () => {
         <h3>About</h3>
         <p>
           The strategy used is entirely algorithm driven, it is connected
-          directly to Binance Markets data. I do not interfere in anything
+          directly to Binance live markets data. I do not interfere in anything
           whatsoever, the bot takes all the decisions.
         </p>
         <p>
@@ -16,16 +16,25 @@ const About = () => {
           will scan the markets and look for pairs with strong upside potential
           and trigger an entry signal when all the indicators used confirm the
           uptrend. Once an entry signal has been sent, no more signals will be
-          sent for the remainder of the trend, so make sure you catch the wave
+          sent for the remainder of the trend, so make sure you jump on the boat
           at the right price before the trend ends.
         </p>
         <p>
-          This is a low frequency strategy, it trades once per trend, and the
-          trade can last from weeks to months.
+          This is a low frequency strategy, it trades at least once per trend,
+          and the trade can last from weeks to months.
         </p>
         <p>
           The strategy will try to avoid trading during pump &amp; dump
           scenarios, however this is not 100% guaranteed.
+        </p>
+        <p>
+          There is no "Take Profit" target, and the "Stop Loss" is dinamically
+          set based on market price action
+        </p>
+        <p>
+          Trades can go wrong. Expect to see trades go as low as -40% in profit,
+          but don't be discouraged by this as one good trade can offset the
+          losses of many bad trades.
         </p>
       </div>
     </Box>

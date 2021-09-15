68fbd026b652c273de399a65dd888e2522ffc754
Jesús Jiménez <jesuswebdev@gmail.com>
2021-09-14 22:24:54 -0400

    add text to about view

--- website/src/views/about/index.js
+++ website/src/views/about/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Box from "@crypto-signals/components/Box";
 
-const About = props => {
+const About = () => {
   return (
     <Box>
       <div className="content is-normal">
-        <h3>About?</h3>
-        Not financial advice
+        <h3>About</h3>
+        <p>
+          Overall indicators include: Trend, Volume, Volatility, Moving
+          Averages. Low frequency strategy. Once an entry signal has been sent,
+          no more signals will be sent for the remainder of the trend (unless
+          the price plummets and the trend is still bullish).
+        </p>
       </div>
     </Box>
   );

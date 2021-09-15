d875f9f1b100fc4efd54698c1df491105036d6c1
Jesús Jiménez <jesuswebdev@gmail.com>
2021-09-15 13:35:21 -0400

    describe the strategy

--- website/src/views/about/index.js
+++ website/src/views/about/index.js
@@ -7,10 +7,25 @@ const About = () => {
       <div className="content is-normal">
         <h3>About</h3>
         <p>
-          Overall indicators include: Trend, Volume, Volatility, Moving
-          Averages. Low frequency strategy. Once an entry signal has been sent,
-          no more signals will be sent for the remainder of the trend (unless
-          the price plummets and the trend is still bullish).
+          The strategy used is entirely algorithm driven, it is connected
+          directly to Binance Markets data. I do not interfere in anything
+          whatsoever, the bot takes all the decisions.
+        </p>
+        <p>
+          The strategy is a "Bullish Trend Following" strategy, this means it
+          will scan the markets and look for pairs with strong upside potential
+          and trigger an entry signal when all the indicators used confirm the
+          uptrend. Once an entry signal has been sent, no more signals will be
+          sent for the remainder of the trend, so make sure you catch the wave
+          at the right price before the trend ends.
+        </p>
+        <p>
+          This is a low frequency strategy, it trades once per trend, and the
+          trade can last from weeks to months.
+        </p>
+        <p>
+          The strategy will try to avoid trading during pump &amp; dump
+          scenarios, however this is not 100% guaranteed.
         </p>
       </div>
     </Box>

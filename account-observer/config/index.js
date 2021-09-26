4129f374a10fc9375b05d762ba9d76b61ea42636
Jesús Jiménez <jesuswebdev@gmail.com>
2021-09-26 15:39:25 -0400

    remove unused env variables

--- account-observer/config/index.js
+++ account-observer/config/index.js
@@ -5,9 +5,6 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 module.exports = {
   db_uri: process.env.DB_URI,
   api_url: process.env.API_URL,
-  redis_uri: process.env.REDIS_URI,
-  exchange: process.env.EXCHANGE,
-  interval: process.env.INTERVAL,
   host: process.env.CANDLES_PROCESSOR_HOST || "localhost",
   port: +process.env.CANDLES_PROCESSOR_PORT || 8080,
   quote_asset: process.env.QUOTE_ASSET
